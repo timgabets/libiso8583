90623fe4de21fe3514125c313e26c1d1408be161
Tim Gabets <tim@gabets.ru>
2018-02-19 22:47:21 +0300

    Using snow.h

--- tests/factorial_testsuite.cc
+++ tests/factorial_testsuite.cc
@@ -1,9 +1,11 @@
-#include "catch.hpp"
+#include "snow.h"
 #include "factorial.hh"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( factorial(1) == 1 );
-    REQUIRE( factorial(2) == 2 );
-    REQUIRE( factorial(3) == 6 );
-    REQUIRE( factorial(10) == 3628800 );
-}
+describe(dummy_test, {
+    it("factorial()", {
+    	asserteq_int( factorial(1), 1 );
+		asserteq_int( factorial(2), 2 );
+		asserteq_int( factorial(3), 6 );
+		asserteq_int( factorial(10), 3628800 );
+    });
+});
