a8b1e99702a4692bd64bdb0c752ad5b5b995e178
Tim Gabets <tim@gabets.ru>
2018-02-21 09:02:59 +0300

    typedef enum lenType

--- include/parser.hh
+++ include/parser.hh
@@ -1,3 +1,39 @@
+
+typedef enum encodingType {
+	BCH,
+	ASCII,
+	BCD
+} encodingType;
+
+typedef enum contentType {
+	CONTENT_TYPE_B,
+	CONTENT_TYPE_N,
+	CONTENT_TYPE_A,
+	CONTENT_TYPE_AN,
+	CONTENT_TYPE_ANS,
+	CONTENT_TYPE_Z,
+} contentType;
+
+typedef enum lenType {
+	FIXED, 
+	LVAR,
+	LLVAR,
+	LLLVAR,
+	LLLLVAR
+} lenType;
+
+//1 :   { 'ContentType':'b',     'MaxLen': 8,   'LenType': LT.FIXED,   'Description': 'Bitmap' },
+typedef struct iso8583field {
+	int length;
+	lenType lengthType;
+	encodingType encType;
+	char description[64] = {0};
+} iso8583field;
+
+iso8583field iso8583fields[1] = {
+
+};
+
 typedef struct iso8583msg {
 	int mti;
 	char PAN[19]; 		// DE 02 - Primary Account Number
