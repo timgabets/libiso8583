32c197ea4535929deddee318f576a4d113aafa39
Tim Gabets <tim@gabets.ru>
2018-02-27 13:33:24 +0300

    Added bitmap

--- include/parser.hh
+++ include/parser.hh
@@ -2,44 +2,44 @@
 #define PARSER_HH
 
 typedef enum lengthType {
-    FIXED, 
-    LVAR,
-    LLVAR,
-    LLLVAR,
-    LLLLVAR
+	FIXED,
+	LVAR,
+	LLVAR,
+	LLLVAR,
+	LLLLVAR
 } lengthType;
 
 typedef enum encodingType {
-    BCH,
-    ASCII,
-    BCD
+	BCH,
+	ASCII,
+	BCD
 } encodingType;
 
 typedef enum contentType {
-    CONTENT_TYPE_B,
-    CONTENT_TYPE_N,
-    CONTENT_TYPE_A,
-    CONTENT_TYPE_AN,
-    CONTENT_TYPE_ANS,
-    CONTENT_TYPE_Z,
+	CONTENT_TYPE_B,
+	CONTENT_TYPE_N,
+	CONTENT_TYPE_A,
+	CONTENT_TYPE_AN,
+	CONTENT_TYPE_ANS,
+	CONTENT_TYPE_Z,
 } contentType;
 
 //1 :   { 'ContentType':'b',     'MaxLen': 8,   'LenType': LT.FIXED,   'Description': 'Bitmap' },
 typedef struct iso8583field {
-    int length;
-    lengthType length_type;
-    encodingType encoding_type;
-    contentType content_type;
-    const char* description;
+	int length;
+	lengthType length_type;
+	encodingType encoding_type;
+	contentType content_type;
+	const char* description;
 } iso8583field;
 
 typedef struct iso8583msg {
-    int mti;
-    char PAN[19];       // DE 02 - Primary Account Number
-    int prcode;         // DE 03 - Processing Code
-    int amount_trxn;    // DE 04 - Transaction Amount
-    int amount_sttl;    // DE 05 - Settlement Amount
-    int amount_billing; // DE 06 - Billing Amount
+	int mti;
+	char PAN[19];       // DE 02 - Primary Account Number
+	int prcode;         // DE 03 - Processing Code
+	int amount_trxn;    // DE 04 - Transaction Amount
+	int amount_sttl;    // DE 05 - Settlement Amount
+	int amount_billing; // DE 06 - Billing Amount
 } iso8583msg;
 
 int parse_message(const char*, iso8583msg*);
--- src/parser.cc
+++ src/parser.cc
@@ -1,6 +1,10 @@
 #include "parser.hh"
+#include "stdio.h"
 
-iso8583field iso8583spec = {2,     FIXED,  BCH,    CONTENT_TYPE_B, "MTI"}; 
+iso8583field iso8583spec[2] = {
+	{2,     FIXED,  BCH,    CONTENT_TYPE_B, "MTI"},
+	{16,    FIXED,  BCH,    CONTENT_TYPE_B, "Bitmap"}
+};
 
 int parse_message(const char* msg, iso8583msg* parsed)
 {
