b3bca1febbb8e62e49c328603fd55e257df97568
Tim Gabets <tim@gabets.ru>
2018-02-27 18:31:42 +0300

    GETBIT() test cases

--- include/bitops.hh
+++ include/bitops.hh
@@ -1,2 +1,3 @@
 
+#define GETBIT( i ) ( (unsigned char) (1 << (7 - (((i) - 1) & 7))) )
 
--- tests/bitops_suite.cc
+++ tests/bitops_suite.cc
@@ -2,9 +2,33 @@
 #include "bitops.hh"
 
 describe(bitops_suite, {
-	subdesc(dummy, {
-		it("dummy", {
-			asserteq_int(1, 0);
+	subdesc(getbit_testcases, {
+		it("GETBIT(1) == 128", {
+			asserteq_int(GETBIT(1), 128);
+		});
+		it("GETBIT(2) == 64", {
+			asserteq_int(GETBIT(2), 64);
+		});
+		it("GETBIT(3) == 32", {
+			asserteq_int(GETBIT(3), 32);
+		});
+		it("GETBIT(4) == 16", {
+			asserteq_int(GETBIT(4), 16);
+		});
+		it("GETBIT(5) == 8", {
+			asserteq_int(GETBIT(5), 8);
+		});
+		it("GETBIT(6) == 4", {
+			asserteq_int(GETBIT(6), 4);
+		});
+		it("GETBIT(7) == 2", {
+			asserteq_int(GETBIT(7), 2);
+		});
+		it("GETBIT(8) == 1", {
+			asserteq_int(GETBIT(8), 1);
+		});
+		it("GETBIT(9) == 128", {
+			asserteq_int(GETBIT(9), 128);
 		});
 	});
 });
